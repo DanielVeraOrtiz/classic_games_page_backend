b68fc7be45a6c8ff4445912db4a9d58c28449c0b
Daniel <di.vera@uc.cl>
2025-12-17 15:59:10 -0300

    Se dejan listas las rutas de login y signup con jwt

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -2,6 +2,9 @@ const Router = require('koa-router');
 var jwt = require('jsonwebtoken');
 const { User } = require('../models');
 const router = new Router();
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 router.post('authentication.signup', '/signup', async (ctx) => {
     try {
@@ -32,6 +35,58 @@ router.post('authentication.signup', '/signup', async (ctx) => {
         console.error('Error: ', err);
         ctx.body = err;
         ctx.status = 400;
+    }
+})
+
+router.post("authentication.login", "/login", async (ctx) => {
+    try {
+        const authInfo = ctx.request.body
+        const user = await User.findOne({ where:{ email:authInfo.email } });
+
+        if (!user) {
+            ctx.body = `The user by the email '${authInfo.email}' was not found`;
+            ctx.status = 400;
+            return;
+        }
+
+        if (!(user.password == authInfo.password)) {
+            ctx.body = "Incorrect email or password";
+            ctx.status = 400;
+            return;
+        }
+
+        // ===================== JWT =====================
+        // Payload: información que se incluye dentro del token.
+        // NO está cifrada, solo firmada.
+        const payload = {
+        scope: ['user']
+        };
+
+        // Secret: clave privada del backend (NUNCA va al cliente)
+        const secret = process.env.JWT_SECRET;
+
+        // Opciones del token
+        const options = {
+            subject: user.id.toString(), // "sub": id del usuario
+            expiresIn: '24h'              // expiración
+        };
+
+        // Firma del JWT:
+        // header + payload + secret -> signature
+        const token = jwt.sign(payload, secret, options);
+        // ==============================================
+
+        ctx.status = 200;
+        ctx.body = {
+        access_token: token,
+        token_type: 'Bearer',
+        expires_in: 24 * 60 * 60,
+        };
+    }
+    catch(error) {
+        ctx.body = error;
+        ctx.status = 400;
+        return;
     }
 })
 
