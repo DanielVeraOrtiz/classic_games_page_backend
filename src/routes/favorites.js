56810633f6649f40762af3698b5b45c22e264bfd
Daniel <di.vera@uc.cl>
2025-12-24 20:38:20 -0300

    Ruta para obtener favorite

--- src/routes/favorites.js
+++ src/routes/favorites.js
@@ -15,7 +15,21 @@ router.get('/me', authMiddleware, async (ctx) => {
         ctx.status = 400;
         ctx.body = err;
     }
-})
+});
+
+router.get('/:gameid', authMiddleware, async (ctx) => {
+    try {
+        const favorite = Favorite.findOne({ where: {
+            user_id: ctx.state.user.sub,
+            game_id: ctx.params.id,
+        }});
+        ctx.body = favorite;
+        ctx.status = 201;
+    } catch (error) {
+        ctx.body = error;
+        ctx.status = 400;
+    }
+});
 
 router.post('/', authMiddleware, async (ctx) => {
     try {
@@ -26,6 +40,28 @@ router.post('/', authMiddleware, async (ctx) => {
         ctx.body = err;
         ctx.status = 400;
     }
-})
+});
+
+router.delete('/:id', authMiddleware, async (ctx) => {
+    try {
+        const deletedCount = await Favorite.destroy({
+            where: {
+                game_id: ctx.params.id,
+                user_id: ctx.state.user.sub,
+             }
+        });
+
+        if (deletedCount === 0) {
+            ctx.status = 404;
+            ctx.body = {error: 'Favorite not fount'}
+            return;
+        }
+
+        ctx.status = 204;
+    } catch (err) {
+        ctx.body = { error: 'Internal server error' };
+        ctx.status = 500;
+    }
+});
 
 module.exports = router;
